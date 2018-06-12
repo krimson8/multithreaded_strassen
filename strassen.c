af56cc0efb2bc3b0f25913a5462829ad47f0bbd9
siahuat0727 <tansiahuat@gmail.com>
2018-06-12 13:22:23 +0800

    indent & clean

--- strassen.c
+++ strassen.c
@@ -8,9 +8,6 @@
 #include <string.h>
 #include <sys/time.h>
 
-
-int n;
-
 typedef struct _Matrix {
     int **v;
     int size;
@@ -21,13 +18,8 @@ typedef struct _ThreadParam {
     int id;
 } ThreadParam;
 
-int thread_count = 4;
-pthread_mutex_t mutexA = PTHREAD_MUTEX_INITIALIZER;
-int counter = 0;
-
-void matrix_create(Matrix *thiz, int size, FILE *fp)
+void matrix_create(Matrix *thiz, int size)
 {
-    assert(thiz != NULL);   
     thiz->size = size;
 
     // dynamic allocate 2d matrix
@@ -35,12 +27,13 @@ void matrix_create(Matrix *thiz, int size, FILE *fp)
     thiz->v = (int**)malloc(size * sizeof(int*));
     for (int i = 0; i < size; ++i)
         thiz->v[i] = arr + i*size;
+}
 
-    if (fp) {
-        for (int i = 0; i < size; ++i)
-            for (int j = 0; j < size; ++j)
-                fscanf(fp, "%d", &(thiz->v[i][j]));
-    }
+void matrix_try_create(Matrix *thiz, int size)
+{
+    assert(thiz != NULL);
+    if (thiz->size == 0)
+        matrix_create(thiz, size);
 }
 
 void matrix_read(Matrix *thiz, FILE *fp)
@@ -48,7 +41,12 @@ void matrix_read(Matrix *thiz, FILE *fp)
     int m, n;
     fscanf(fp, "%d %d", &m, &n);
     assert(m == n);
-    matrix_create(thiz, m, fp);
+    matrix_try_create(thiz, m);
+    if (fp) {
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < m; ++j)
+                fscanf(fp, "%d", &(thiz->v[i][j]));
+    }
 }
 
 void matrix_print(Matrix thiz)
@@ -61,17 +59,11 @@ void matrix_print(Matrix thiz)
     puts("");
 }
 
-void matrix_try_create(Matrix *thiz, int size, FILE *fp)
-{
-    if (thiz->size == 0)
-        matrix_create(thiz, size, NULL);
-}
-
 void matrix_check_AB_try_create_C(const Matrix *A, const Matrix *B, Matrix *C)
 {
     assert(A != NULL && B != NULL);
     assert(A->size == B->size);
-    matrix_try_create(C, A->size, NULL);
+    matrix_try_create(C, A->size);
 }
 
 void matrix_add(const Matrix *A, const Matrix *B, Matrix *C)
@@ -82,7 +74,7 @@ void matrix_add(const Matrix *A, const Matrix *B, Matrix *C)
         for (int j = 0; j < size; ++j) {
             C->v[i][j] = A->v[i][j] + B->v[i][j];
         }
-    } 
+    }
 }
 
 void matrix_sub(const Matrix *A, const Matrix *B, Matrix *C)
@@ -93,7 +85,7 @@ void matrix_sub(const Matrix *A, const Matrix *B, Matrix *C)
         for (int j = 0; j < size; ++j) {
             C->v[i][j] = A->v[i][j] - B->v[i][j];
         }
-    } 
+    }
 }
 
 void matrix_mul(const Matrix *A, const Matrix *B, Matrix *C)
@@ -107,28 +99,28 @@ void matrix_mul(const Matrix *A, const Matrix *B, Matrix *C)
                 C->v[i][j] += A->v[i][k] * B->v[k][j];
             }
         }
-    } 
+    }
 }
 
-void matrix_divide_4(Matrix *thiz, Matrix *block)
+void matrix_divide_4(const Matrix *thiz, Matrix *block)
 {
     int size = thiz->size;
     int size_divided = size/2;
     for (int i = 0; i < 4; ++i)
-        matrix_create(&block[i], size_divided, 0);
+        matrix_try_create(&block[i], size_divided);
     for (int j = 0; j < size_divided; ++j) {
-        block[0].v[j] = thiz->v[j]; // TODO double allocate
+        block[0].v[j] = thiz->v[j]; // TODO memory leak !
         block[1].v[j] = thiz->v[j] + size_divided;
         block[2].v[j] = thiz->v[j+size_divided];
         block[3].v[j] = thiz->v[j+size_divided] + size_divided;
     }
 }
 
-void matrix_combine_4(Matrix *thiz, Matrix *block)
+void matrix_combine_4(Matrix *thiz, const Matrix *block)
 {
     int size_divided = block[0].size;
     int size= 2*size_divided;
-    matrix_try_create(thiz, size, NULL);
+    matrix_try_create(thiz, size);
     for (int i = 0; i < size_divided; ++i) {
         for (int j = 0; j < size_divided; ++j) {
             thiz->v[i][j] = block[0].v[i][j];
@@ -146,34 +138,27 @@ void _strassen_mul(const Matrix *A, const Matrix *B, Matrix *M, Matrix *tmp, con
         matrix_add(&B[0], &B[3], &tmp[1]);
         matrix_mul(&tmp[0], &tmp[1], &M[0]);
     }
-
     if (id == -1 || id == 1) {
         matrix_add(&A[2], &A[3], &tmp[2]);
         matrix_mul(&tmp[2], &B[0], &M[1]);
     }
-
     if (id == -1 || id == 2) {
         matrix_sub(&B[1], &B[3], &tmp[3]);
         matrix_mul(&A[0], &tmp[3], &M[2]);
     }
-
     if (id == -1 || id == 3) {
         matrix_sub(&B[2], &B[0], &tmp[4]);
         matrix_mul(&A[3], &tmp[4], &M[3]);
     }
-
-
     if (id == -1 || id == 4) {
         matrix_add(&A[0], &A[1], &tmp[5]);
         matrix_mul(&tmp[5], &B[3], &M[4]);
     }
-
     if (id == -1 || id == 5) {
         matrix_sub(&A[2], &A[0], &tmp[6]);
         matrix_add(&B[0], &B[1], &tmp[7]);
         matrix_mul(&tmp[6], &tmp[7], &M[5]);
     }
-
     if (id == -1 || id == 6) {
         matrix_sub(&A[1], &A[3], &tmp[8]);
         matrix_add(&B[2], &B[3], &tmp[9]);
@@ -183,17 +168,17 @@ void _strassen_mul(const Matrix *A, const Matrix *B, Matrix *M, Matrix *tmp, con
 
 void* pthread_func(void *arg)
 {
-    ThreadParam *t = (ThreadParam*)arg; 
+    ThreadParam *t = (ThreadParam*)arg;
     _strassen_mul(t->A, t->B, t->M, t->tmp, t->id);
     pthread_exit(NULL);
 }
 
 void strassen_mul(Matrix *A_all, Matrix *B_all, Matrix *C_all, bool parallel)
 {
-    Matrix A[4] = {0}, 
-           B[4] = {0}, 
-           C[4] = {0}, 
-           M[7] = {0}, 
+    Matrix A[4] = {0},
+           B[4] = {0},
+           C[4] = {0},
+           M[7] = {0},
            tmp[10] = {0};
 
     matrix_divide_4(A_all, A);
@@ -228,9 +213,8 @@ void strassen_mul(Matrix *A_all, Matrix *B_all, Matrix *C_all, bool parallel)
     matrix_combine_4(C_all, C);
 }
 
-int main(int argc, const char **argv) 
-{ 
-
+int main(int argc, const char **argv)
+{
     if (argc != 3) {
         puts("./main path/to/test/data type_of_mul");
         exit(EXIT_FAILURE);
@@ -243,8 +227,8 @@ int main(int argc, const char **argv)
     }
     int mul_type = atoi(argv[2]);
 
-    Matrix A = {0}, 
-           B = {0}, 
+    Matrix A = {0},
+           B = {0},
            C = {0};
     memset(&A, 0, sizeof(Matrix));
     memset(&B, 0, sizeof(Matrix));
@@ -266,11 +250,14 @@ int main(int argc, const char **argv)
     case 2:
         strassen_mul(&A, &B, &C, true);
         break;
+    default:
+        fprintf(stderr, "wrong mul type (argv[2])");
+        exit(EXIT_FAILURE);
     }
 
     gettimeofday(&end, NULL);
     int time_spent = (1e6) * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+    printf("%d ms\n",time_spent/1000);
 
-    printf("%d ms\n",time_spent);    
     return 0;
 }
